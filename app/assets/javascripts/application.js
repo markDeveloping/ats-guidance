b241bc18040825dbc8fb4856596384a63644f0f6
markDeveloping <mark.developing@gmail.com>
2016-04-13 09:14:28 +0100

    Added back to contents functionality, and completed create an account section.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,48 +17,17 @@
 
 
 $(document).ready(function(){
-	/*Capture all of the variables for working conditional statements*/
-	function variables() {
-   blindSift = $('input[name="blind-sift"]:checked').val();
-   cvCurrent = $('input[name="CV-upload"]:checked').val();
-   supportCurrent = $('input[name="supporting-upload"]:checked').val();
-   cvNew = $('input[name="CV-new"]:checked').val();
-   skillsNew = $('input[name="skills-new"]:checked').val();
-	};
-
-	function addRemove() {	
-	/*If new CV do you want to include a section for skills?*/
-		if (blindSift === "No") {
-			$('.skills-optional').addClass('hidden');
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 590) {
+			$('.back').fadeIn();
+		} else {
+			$('.back').fadeOut();
 		}
-		else if(cvNew === "No" || cvNew === "Not selected") {
-			$('.skills-optional').addClass('hidden');
-		}
-		else {
-			$('.skills-optional').removeClass('hidden');
-		};
-	/*If old additional attachment is yes, provide details*/
-		if(supportCurrent === "No" || supportCurrent === "Not selected"){
-			$('.enter-details').addClass('hidden');
-		}
-		else {
-			$('.enter-details').removeClass('hidden');
-		};
-	/*If yes and not select show new section triggers*/
-		if(blindSift === "Yes" || blindSift === "Not selected"){
-			$('.old').addClass('hidden');
-			$('.new').removeClass('hidden');
-		}
-		else {
-			$('.new').addClass('hidden');
-			$('.old').removeClass('hidden');
-		};
-	};
-
-	$('input').click(function(){
-		variables();
-		addRemove();
 	});
-		variables();
-		addRemove();
-});
+
+	$('.back').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+})
